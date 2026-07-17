6cf631e5b44f4e210dbe93c852c3bfb89644edd5
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-07-17 09:29:28 -0300

    Capturando as palavras

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const { error } = require('console');
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro,texto) => {
-    console.log(texto);
+    capturaPalavra(texto)
 });
+
+
+function capturaPalavra(texto){
+    const listapalavras = texto.split(" ");
+    const resultado = {};
+
+    listapalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    });
+
+    console.log(resultado);
+}
